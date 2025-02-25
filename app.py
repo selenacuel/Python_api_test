de78068c03169c9e53e8e004eafb277b8714a2c3
selenacuel <Selenacuel@gmail.com>
2025-02-25 04:38:00 +0000

    llegamos a agregar el endpoint "nuevo" funciona casi todo, pero colores y detalles esta mal! y algun tema de backslash

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import os
 import sys
 import traceback
+import json
 
 app = Flask(__name__)
 
@@ -53,7 +54,79 @@ def health():
     Retorna: "HTTP 200 - OK | Estoy Funcionando"
     """
     return "HTTP 200 - OK | Estoy Funcionando"
+# No tocar Arriba
+# ----------------------------------
 
+@app.route("/nuevo")
+def nuevo():
+    """
+    Endpoint nuevo sirve para crear un nuevo perfil de personaje
+    utilizar el formato de /plantilla/perfil.json, cada perfil tiene un id,
+    el id tambien es normbre de archivo. ej. id: 0 -> 0.json
+    1. Leer Plantilla
+    2. Recibir datos
+    3. Cargar Plantilla
+    4. Escribir pefirl nuevo
+    5. Devolver ID de perfil y Exito, sino ERROR!
+
+    ARG: ?Nombre=""&Edad=""&Foto=""&Creador=""&Sexo=""&Colores=["","",""]&Tecnicas:""&Raza:""&Vivo:true
+    """
+
+    # abrir plantilla y convertirla en diccionario
+    with open('plantilla/perfil.json', 'r') as file:
+        plantilla = json.load(file)
+    
+    # Leer arguentos y asignarlos a variables
+    nombre = request.args.get("Nombre")
+    edad = request.args.get("Edad")
+    foto = request.args.get("Foto")
+    creador = request.args.get("Creador")
+    sexo = request.args.get("Sexo")
+    colores = request.args.get("Colores")
+    tecnicas = request.args.get("Tecnicas")
+    raza = request.args.get("Raza")
+    vivo = request.args.get("Vivo")
+
+    # completamos la plantilla
+
+    plantilla["Nombre"] = nombre
+    plantilla["Edad"] = edad
+    plantilla["Sexo"] = sexo
+    plantilla["Foto"] = foto
+    plantilla["Creador"] = creador
+    plantilla["Detalles"]["Tecnicas"] = tecnicas
+    plantilla["Detalles"]["Raza"] = raza
+    plantilla["Detalles"]["Vivo"] = vivo
+
+    # encontrar si existe algun perfil y cual es el ultimo
+    ultimo_perfil=0
+    for perfil in os.listdir('perfiles'):
+        if perfil.endswith(".json"):
+            try:
+                numero = int(perfil[:-5]) # removemos extension .json
+                ultimo_perfil = max(ultimo_perfil,perfil)
+            except ValueError:
+                pass # Ignorar si no hay archivo
+    ultimo_perfil = ultimo_perfil + 1
+    
+    plantilla["id"] = ultimo_perfil
+    
+    # demostramos que encontramos los datos
+    print(f"Nombre: {nombre}, Edad: {edad}, Sexo: {sexo}")
+
+    # grabar la plantilla como el nuevo perfil 
+    nuevo_perfil = os.path.join('perfiles',f"{ultimo_perfil}.json") # armamos el nombre del archivo
+
+    with open(nuevo_perfil, "w", encoding="utf-8") as perfil_json:
+        json.dump(plantilla, perfil_json, indent=4, ensure_ascii=False) #Lo guardamos
+
+    # avisamos! 
+
+    return f"Perfil ID: {ultimo_perfil} fue creado exitosamente!"
+
+
+# ----------------------------------
+# No tocar abajo
 if __name__ == "__main__":
     print("Escaneando código antes de iniciar la aplicación...")
     scan_code()
